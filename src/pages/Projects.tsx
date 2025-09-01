83cbcfa1e02157f5d4ae19d6a172484e3f39b6be
RuntyAxis1111 <johan.martinez@hybecorp.com>
2025-08-31 23:33:54 -0600

    Fix useState import in Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,5 @@
-import { Link, useState } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Music, BarChart3, Users, Megaphone, ExternalLink, ChevronDown, Search, Hash } from 'lucide-react'
 import { projects } from '../lib/dashboards'
 
